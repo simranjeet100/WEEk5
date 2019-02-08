98a5ec2f77ed1cdb8e77abdad0cfcd3430d4bf74
visualstudio <visualstudio@122-PC28>
2019-02-08 18:00:06 -0500

    1

---
+++ c0727100week05/c0727100week05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0727100week05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 7;
+           
+        }
+    }
+}
